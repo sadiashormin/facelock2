ddb686142f2a433ea04febfa75907c695d22664f
sadiashormin <mohammadmahboburrahman@Mohammads-MacBook-Pro.local>
2018-04-01 13:54:00 -0500

    add library

--- facelockproj/home/views.py
+++ facelockproj/home/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 
 from home.forms import HomeForm
 from home.models import Post, Friend
+import face_recognition
+import os
 
 
 class HomeView(TemplateView):
@@ -34,9 +36,49 @@ class HomeView(TemplateView):
             post = form.save(commit=False)
             post.user = request.user
             post.save()
-
             text = form.cleaned_data['post']
             form = HomeForm()
+
+            friendsPicFileName = ['mahbub.jpg', 'sadia.jpg', 'shaila.jpg']
+            uploadedPhotoName = "sadiamahbub.jpg"
+            uploadedPhoto = face_recognition.load_image_file(os.path.abspath(os.path.dirname(__file__))+"/static/"+uploadedPhotoName)
+            uploadedPhotoEncodlings = face_recognition.face_encodings(uploadedPhoto)
+            for unknownFaceEncoding in uploadedPhotoEncodlings:
+                for friendPicFileName in friendsPicFileName:
+                    fndpic = face_recognition.load_image_file(os.path.abspath(os.path.dirname(__file__))+"/static/"+friendPicFileName)
+                    friendpicEncoding = face_recognition.face_encodings(fndpic)[0]
+                    results = face_recognition.compare_faces([unknownFaceEncoding], friendpicEncoding)
+
+                    if results[0] == True:
+                        b=2
+                    else:
+                        c=3
+
+            # picture_of_me = face_recognition.load_image_file(
+            #     os.path.abspath(os.path.dirname(__file__))+"/static/sadia.jpg")
+            # # face_locations = face_recognition.face_locations(picture_of_me)
+
+            # # picture_of_me = face_recognition.load_image_file("me.jpg")
+            # my_face_encoding = face_recognition.face_encodings(picture_of_me)[
+            #     0]
+
+            # # my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
+
+            # unknown_picture = face_recognition.load_image_file(
+            #     os.path.abspath(os.path.dirname(__file__))+"/static/sadiamahbub.jpg")
+            # unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[
+            #     1]
+
+            # # Now we can see the two face encodings are of the same person with `compare_faces`!
+
+            # results = face_recognition.compare_faces(
+            #     [my_face_encoding], unknown_face_encoding)
+
+            # if results[0] == True:
+            #     print("It's a picture of me!")
+            # else:
+            #     print("It's not a picture of me!")
+
             return redirect('home:home')
 
         args = {'form': form, 'text': text}
